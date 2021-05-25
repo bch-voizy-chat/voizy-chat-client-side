71c8269a4af3793f01dfe3aa4d8f84d4e1f0d4bb
laurielim <laurie.limsam@gmail.com>
2021-05-25 17:52:28 +0300

    Create pages

---
+++ src/pages/NewAudio.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewAudio = () => {
+	return <div>New Audio</div>;
+};
+
+export default NewAudio;
---
+++ src/pages/SingleThread.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SingleThread = () => {
+	return <div className='content'>Conversation</div>;
+};
+
+export default SingleThread;
