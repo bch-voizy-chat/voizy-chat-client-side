5c69db84844b01d615b7afe7cd57859ce2d09fdf
laurielim <laurie.limsam@gmail.com>
2021-05-26 13:05:43 +0300

    add sign in and sign up forms

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useRef } from "react";
+import { Form, Button } from "react-bootstrap";
 
 const Login = () => {
-	return <div>Login</div>;
+	const emailRef = useRef();
+	const passwordRef = useRef();
+	return (
+		<div
+			className='flow d-flex flex-column justify-content-center mx-auto'
+			style={{ minHeight: "80vh", maxWidth: "320px" }}
+		>
+			<h2 className='text-center'>Login:</h2>
+			<Form className='flow form-flow'>
+				<Form.Group id='email'>
+					<Form.Label>Email</Form.Label>
+					<Form.Control type='email' ref={emailRef} required />
+				</Form.Group>
+				<Form.Group id='password'>
+					<Form.Label>Password</Form.Label>
+					<Form.Control type='password' ref={passwordRef} required />
+				</Form.Group>
+
+				<Button type='submit' className='w-100 mt-3'>
+					Create Account
+				</Button>
+			</Form>
+			<p className='w-100 text-center'>
+				Don't have an account?
+				<br />
+				Create one here.
+			</p>
+		</div>
+	);
 };
 
 export default Login;
