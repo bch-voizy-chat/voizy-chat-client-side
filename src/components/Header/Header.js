efa0a41b0d77cecf81dfadfed729707e9d1721f1
laurielim <laurie.limsam@gmail.com>
2021-05-26 11:52:27 +0300

    add logo to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+
+import logo from "../../assets/Logo-white-png-tinified.png";
 
 const Header = () => {
 	/**for dev purpose */
@@ -7,8 +10,10 @@ const Header = () => {
 	return (
 		<header className='d-flex justify-content-between'>
 			<div className='logo'>
-				<h1 className='visually-hidden'>Voizy Chat</h1>
-				<img src='none' alt='Voizy chat logo.' />
+				<NavLink to='/'>
+					<h1 className='visually-hidden'>Voizy Chat</h1>
+					<img src={logo} alt='Voizy Logo' />
+				</NavLink>
 			</div>
 			{currentUser ? <div>User icon</div> : <div>Log in/Register</div>}
 		</header>
