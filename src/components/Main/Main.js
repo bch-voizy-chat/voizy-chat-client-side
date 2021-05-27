97e3d08cbce3098c97b4d21b1dd4837eb6ff2c86
laurielim <laurie.limsam@gmail.com>
2021-05-27 14:59:56 +0300

    create login and signup handlers

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -16,7 +16,6 @@ const Main = () => {
 				<Route exact path='/' component={Home} />
 				<Route exact path='/login' component={Login} />
 				<Route exact path='/signup' component={Signup} />
-
 				<Route exact path='/new' component={NewAudio} />
 				<Route exact path='/conversation/:id' component={SingleThread} />
 				<Route path='/about' component={About} />
