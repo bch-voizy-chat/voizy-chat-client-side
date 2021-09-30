e6b65bd3fbd451f9d0308461e1966cf68270511e
laurielim <laurie.limsam@gmail.com>
2021-09-30 14:57:14 +0300

    Update date format

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,11 +1,52 @@
-export const formatDate = (date) => {
-	let d = new Date(date);
-	let ye = new Intl.DateTimeFormat("en", { year: "numeric" }).format(d);
-	let mo = new Intl.DateTimeFormat("en", { month: "short" }).format(d);
-	let da = new Intl.DateTimeFormat("en", { day: "2-digit" }).format(d);
-	let hr = d.getHours();
-	let min = d.getMinutes().toString().padStart(2, "0");
-	return `${da}/${mo}/${ye}, ${hr}:${min}`;
+const getFormattedDate = (date, prefomattedDate = false, hideYear = false) => {
+	const day = date.getDate();
+	const month = new Intl.DateTimeFormat("en", { month: "short" }).format(date);
+	const year = date.getFullYear();
+	const hours = date.getHours();
+	const minutes = date.getMinutes().toString().padStart(2, "0");
+
+	if (prefomattedDate) {
+		// Today at 10:20
+		// Yesterday at 10:20
+		return `${prefomattedDate} at ${hours}:${minutes}`;
+	}
+	if (hideYear) {
+		// Jan 10
+		return `${month} ${day}`;
+	}
+	// Jan 10, 2021
+	return `${month} ${day}, ${year}`;
+};
+export const formatDate = (dateParam) => {
+	if (!dateParam) {
+		return "Oct 21, 2015";
+	}
+
+	const date = typeof dateParam === "object" ? dateParam : new Date(dateParam);
+	const DAY_IN_MS = 86400000; // 24 * 60 * 60 * 1000
+	const today = new Date();
+	const yesterday = new Date(today - DAY_IN_MS);
+	const seconds = Math.round((today - date) / 1000);
+	const minutes = Math.round(seconds / 60);
+	const isToday = today.toDateString() === date.toDateString();
+	const isYesterday = yesterday.toDateString() === date.toDateString();
+	const isThisYear = today.getFullYear() === date.getFullYear();
+
+	if (seconds < 5) {
+		return "now";
+	} else if (seconds < 60) {
+		return `${seconds} seconds ago`;
+	} else if (minutes < 60) {
+		return `${minutes} minutes ago`;
+	} else if (isToday) {
+		return getFormattedDate(date, "Today"); // Today at 10:20
+	} else if (isYesterday) {
+		return getFormattedDate(date, "Yesterday"); // Yesterday at 10:20
+	} else if (isThisYear) {
+		return getFormattedDate(date, false, true); // Jan 10
+	}
+
+	return getFormattedDate(date); // Jan 10, 2021 at 10:20
 };
 
 export const formatTags = (tags) => {
